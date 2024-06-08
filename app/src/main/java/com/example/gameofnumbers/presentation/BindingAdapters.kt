d5716cf7157503fbc5fed4aca944d8a914f9d752
Роман Савельев <r.zrk@yandex.ru>
2024-06-08 17:23:52 +0500

    DataBinding для ViewModel со слушателями нажатий и подпиской на данные

--- app/src/main/java/com/example/gameofnumbers/presentation/BindingAdapters.kt
+++ app/src/main/java/com/example/gameofnumbers/presentation/BindingAdapters.kt
@@ -1,10 +1,18 @@
 package com.example.gameofnumbers.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.example.gameofnumbers.R
 import com.example.gameofnumbers.domain.entity.GameResult
+
+interface OnOptionClickListener {
+    fun onOptionClick(option: Int)
+}
 
 @BindingAdapter("requireAnswers")
 fun bindRequireAnswers(view: TextView, count: Int) {
@@ -31,6 +39,28 @@ fun bindScorePercentage(view: TextView, gameResult: GameResult) {
         view.context.getString(R.string.score_percentage, getPercentageOfRightAnswers(gameResult))
 }
 
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(view: TextView, number: Int) {
+    view.text = number.toString()
+}
+
+@BindingAdapter("enoughCount")
+fun bindEnoughCount(view: TextView, enough: Boolean) {
+    view.setTextColor(getColorByState(view.context, enough))
+}
+
+@BindingAdapter("enoughPercent")
+fun bindEnoughPercent(progressBar: ProgressBar, enough: Boolean) {
+    progressBar.progressTintList = ColorStateList.valueOf(getColorByState(progressBar.context, enough))
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(view: TextView, clickListener: OnOptionClickListener) {
+    view.setOnClickListener {
+        clickListener.onOptionClick(view.text.toString().toInt())
+    }
+}
+
 private fun getPercentageOfRightAnswers(gameResult: GameResult) = with(gameResult) {
     if (countOfQuestions == 0) {
         0
@@ -43,4 +73,13 @@ private fun getEmoji(winner: Boolean) = if (winner) {
     R.drawable.ic_smile
 } else {
     R.drawable.ic_sad
+}
+
+private fun getColorByState(context: Context, goodState: Boolean): Int {
+    val colorResId = if (goodState) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
 }
